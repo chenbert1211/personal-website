60aa33c3855b2e76596fbe1140f7abc0fc06c776
chenbert1211 <chenbert1211@gmail.com>
2025-01-24 09:47:04 -0600

    format wording for projects

--- src/app/components/mid-components/Projects.js
+++ src/app/components/mid-components/Projects.js
@@ -1,93 +1,112 @@
-
 "use client"
 import React, { useState } from "react";
 import Image from "next/image";
+
 export default function Projects() {
-    const projects = [
-        {
-          title: "APPRAISED",
-          role: "Team Lead/ Full Stack Developer",
-          date: "Sep/2022",
-          links: {
-            demo: "https://youtu.be/FKAb1YgcVsw",
-            github: "https://github.com/2206-capstone-wbny/2206-Capstone-Appraised"
-          },
-          description: [
-            "Led a team of four developers to create a real-estate intelligence platform, making property data easily accessible to everyday users.",
-            "Implemented custom algorithms for property valuation, analyzing performance metrics to provide accurate appraisals.",
-            "Utilized object-oriented programming to ensure maintainable, scalable code and improve overall system performance."
-          ]
-        },
-        {
-          title: "No Bare Feet",
-          role: "Full Stack Developer",
-          date: "Aug/2022",
-          links: {
-            github: "https://github.com/2206-fsa-gs-team-shoe-shop/shoe-shop"
-          },
-          description: [
-            "Developed a collectible sneaker e-commerce platform using PostgreSQL, Express.js, RESTful APIs, and React/Redux.",
-            "Collaborated with a team of four developers to design and implement key features.",
-            "Led design of UI components and backend functions, ensuring a seamless and responsive user experience."
-          ]
-        },
-        {
-          title: "Dungeon Attack",
-          role: "Full Stack Developer",
-          date: "Jun/2022",
-          links: {
-            demo: "https://www.youtube.com/watch?v=-w38spqeeMk"
-          },
-          description: [
-            "Integrated multiple enemy intelligence and animation in regards of player movement.",
-            "Created solely with HTML canvas and JavaScript.",
-            "Implemented gravity, movement, actions, and animation to improve player enjoyment."
-          ]
-        }
-      ];
+  const projects = [
+    {
+      title: "APPRAISED",
+      role: "Team Lead/ Full Stack Developer",
+      summary: `As the Team Lead for a real-estate intelligence platform, I guided four developers in creating a robust system that makes property data accessible to everyday users. By implementing advanced algorithms and analyzing performance metrics, we delivered accurate and timely appraisals for improved decision-making. Leveraging object-oriented programming principles ensured our code remained maintainable and scalable as the project expanded. This end-to-end approach significantly streamlined appraisal processes and helped users gain valuable real-estate insights.`,
+      date: "Sep/2022",
+      links: {
+        demo: "https://youtu.be/FKAb1YgcVsw",
+        github: "https://github.com/2206-capstone-wbny/2206-Capstone-Appraised"
+      },
+      description: [
+        "Led a team of four developers to create a real-estate intelligence platform, making property data easily accessible to everyday users.",
+        "Implemented custom algorithms for property valuation, analyzing performance metrics to provide accurate appraisals.",
+        "Utilized object-oriented programming to ensure maintainable, scalable code and improve overall system performance."
+      ]
+    },
+    {
+      title: "No Bare Feet",
+      role: "Full Stack Developer",
+      summary: `I contributed to an e-commerce platform dedicated to collectible sneakers, combining PostgreSQL, Express.js, RESTful APIs, and React/Redux to build a robust solution. Collaborating closely with four fellow developers, we integrated advanced features such as secure authentication and intuitive shopping cart functionality. I led the design of crucial UI components and backend systems, ensuring seamless performance across diverse devices. Our team’s agile approach and coordinated efforts resulted in a user-centric platform that catered to sneaker enthusiasts looking for a smooth and responsive shopping experience.`,
+      date: "Aug/2022",
+      links: {
+        github: "https://github.com/2206-fsa-gs-team-shoe-shop/shoe-shop"
+      },
+      description: [
+        "Developed a collectible sneaker e-commerce platform using PostgreSQL, Express.js, RESTful APIs, and React/Redux.",
+        "Collaborated with a team of four developers to design and implement key features.",
+        "Led design of UI components and backend functions, ensuring a seamless and responsive user experience."
+      ]
+    },
+    {
+      title: "Dungeon Attack",
+      role: "Full Stack Developer",
+      summary: `Dungeon Attack is a dynamic HTML canvas-based game that I built from the ground up with JavaScript. I integrated multiple enemy AI routines and animation systems, enhancing realism in how enemies respond to player movement. By implementing gravity, movement, actions, and particle effects, the game delivers a fast-paced and visually engaging experience. This project honed my understanding of real-time rendering and responsive controls, culminating in an immersive environment that keeps players entertained.`,
+      date: "Jun/2022",
+      links: {
+        demo: "https://www.youtube.com/watch?v=-w38spqeeMk"
+      },
+      description: [
+        "Integrated multiple enemy intelligence and animation in regards of player movement.",
+        "Created solely with HTML canvas and JavaScript.",
+        "Implemented gravity, movement, actions, and animation to improve player enjoyment."
+      ]
+    }
+  ];
 
-      const [currentIndex, setCurrentIndex] = useState(0);
+  const [currentIndex, setCurrentIndex] = useState(0);
 
-      const nextProject = () => {
-        if (currentIndex < projects.length - 1) {
-          setCurrentIndex(currentIndex + 1);
-        }
-      };
-    
-      // Previous project
-      const prevProject = () => {
-        if (currentIndex > 0) {
-          setCurrentIndex(currentIndex - 1);
-        }
-      };
+  const nextProject = () => {
+    if (currentIndex < projects.length - 1) {
+      setCurrentIndex(currentIndex + 1);
+    }
+  };
 
+  const prevProject = () => {
+    if (currentIndex > 0) {
+      setCurrentIndex(currentIndex - 1);
+    }
+  };
 
-    return (
-        <div className="projects-container">
-      <h1 className="title">PROJECTS</h1>
-
+  return (
+    <div className="projects-container">
+      <div className="proj-line">
+      <h1 className="title-proj">PROJECTS</h1>
+      </ div>
       <div className="project-slide">
-        <h2 className="project-name"><strong>{projects[currentIndex].title}</strong></h2>
-        <p className="proj-disc"><strong>{projects[currentIndex].role}</strong> | {projects[currentIndex].date}</p>
-        
+        <h2 className="project-name">
+          <strong>{projects[currentIndex].title}</strong>
+        </h2>
+        <p className="proj-disc">
+          <strong>{projects[currentIndex].role}</strong> | {projects[currentIndex].date}
+        </p>
+        <p className="proj-summary">{projects[currentIndex].summary}</p>
+
         {/* Links */}
         <div className="icon-container">
           {projects[currentIndex].links.demo && (
-            <a href={projects[currentIndex].links.demo} target="_blank" rel="noopener noreferrer">
-              <Image src='/yt-logo.jpg'
-            className="icon-img"
-            alt = 'yt'
-            width={300}
-            height={300}/>
+            <a
+              href={projects[currentIndex].links.demo}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Image
+                src="/yt-logo.jpg"
+                className="icon-img"
+                alt="yt"
+                width={300}
+                height={300}
+              />
             </a>
           )}
           {projects[currentIndex].links.github && (
-            <a href={projects[currentIndex].links.github} target="_blank" rel="noopener noreferrer">
-              <Image src='/git-logo.png'
-            className="icon-img"
-            alt = 'github'
-            width={300}
-            height={300}/>
+            <a
+              href={projects[currentIndex].links.github}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Image
+                src="/git-logo.png"
+                className="icon-img"
+                alt="github"
+                width={300}
+                height={300}
+              />
             </a>
           )}
         </div>
@@ -102,9 +121,17 @@ export default function Projects() {
 
       {/* Navigation Buttons */}
       <div className="navigation-buttons">
-        <a onClick={prevProject} disabled={currentIndex === 0} className="next">&#8249;</a>
-        <a onClick={nextProject} disabled={currentIndex === projects.length - 1} className="next">&#8250;</a>
+        <a onClick={prevProject} disabled={currentIndex === 0} className="next">
+          &#8249;
+        </a>
+        <a
+          onClick={nextProject}
+          disabled={currentIndex === projects.length - 1}
+          className="next"
+        >
+          &#8250;
+        </a>
       </div>
     </div>
-    );
-  }
+  );
+}
