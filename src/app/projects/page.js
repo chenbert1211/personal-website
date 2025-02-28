0990924dd17f6592e2d098647aa7b93871ab217a
chenbert1211 <chenbert1211@gmail.com>
2025-02-28 13:32:39 -0600

    work in progress for contacts, bio, and projects

---
+++ src/app/projects/page.js
@@ -0,0 +1,8 @@
+export default function Projects() {
+    return (
+      <div className="wip-container">
+        <img src="/WIP.gif" alt="Work in Progress" className="wip-gif" />
+        <h1 className="wip-title">WORK IN PROGRESS</h1>
+      </div>
+    );
+  }
